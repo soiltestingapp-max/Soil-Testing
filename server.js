9258916cfaebd649808ada1840069ce6b01f5678
Divyang Chunara <divyangchunara@192.168.1.6>
2026-01-12 20:02:57 +0530

    Fix Render port binding

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import adminRoutes from "./routes/adminRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
